b641135a57af1f842c12a110749625595e429dcb
webcrazy003 <raycoaster99001@gmail.com>
2020-11-09 08:56:20 -0500

    Integrate angular-froala library

--- src/froala-angular/src/app/app.module.ts
+++ src/froala-angular/src/app/app.module.ts
@@ -1,6 +1,24 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+// Import all Froala Editor plugins.
+// import 'froala-editor/js/plugins.pkgd.min.js';
+
+// Import a single Froala Editor plugin.
+// import 'froala-editor/js/plugins/align.min.js';
+
+// Import a Froala Editor language file.
+// import 'froala-editor/js/languages/de.js';
+
+// Import a third-party plugin.
+// import 'froala-editor/js/third_party/font_awesome.min';
+// import 'froala-editor/js/third_party/image_tui.min';
+// import 'froala-editor/js/third_party/spell_checker.min';
+// import 'froala-editor/js/third_party/embedly.min';
+
+// Import Angular plugin.
+import { FroalaEditorModule, FroalaViewModule } from 'angular-froala-wysiwyg';
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
@@ -10,7 +28,9 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FroalaEditorModule.forRoot(),
+    FroalaViewModule.forRoot()
   ],
   providers: [],
   bootstrap: [AppComponent]
