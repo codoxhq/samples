5d743792e5d67aa0cc0fdb1cbc885e1ceb775e48
David Sun <david@codox.io>
2020-12-16 14:24:29 -0800

    update doc id

--- react-froala-codox/src/App.js
+++ react-froala-codox/src/App.js
@@ -9,8 +9,8 @@ function App() {
   const apiKey = '58e429b0-be4a-4cd8-8c8d-9a37fb0adec0';
   const username = 'Chris';
   const docs = [
-    { "id": "doc1", "content": "Hello World" },
-    { "id": "doc2", "content": "One two three" }
+    { "id": "8d52d64b-f7c5-40de-8137-629376ffbc76", name: "doc1", "content": "Hello World" },
+    { "id": "'8d52d64b-f7c5-40de-8137-629376ffbc77", name: "doc2", "content": "One two three" }
   ];
   const [activeDoc, setActiveDoc] = useState(docs[0])
   const [codox, setCodoxInstance] = useState(null)
@@ -56,7 +56,7 @@ function App() {
             <ul className="document-list">
               {docs.map(d =>
                 <li key={d.id} onClick={() => changeActiveDoc(d)}>
-                  <a className={`document-link ${activeDoc.id === d.id && 'active'}`}>{d.id}</a>
+                  <a className={`document-link ${activeDoc.id === d.id && 'active'}`}>{d.name}</a>
                 </li>
               )}
 
