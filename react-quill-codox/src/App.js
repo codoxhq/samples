ac3c9412a489cc0b4c459689ff32c4c76ba615c3
vanyakulinich <ivan@codox.io>
2024-08-06 10:09:25 +0300

    [update]: react-quill demo with quill-provider

--- react-quill-codox/src/App.js
+++ react-quill-codox/src/App.js
@@ -1,49 +1,44 @@
-import { useState, useEffect } from 'react'
-import RealTimeQuillEditor from './components/RealTimeQuillEditor'
+import { useState, useEffect } from 'react';
+import RealTimeQuillEditor from './components/RealTimeQuillEditor';
 import './App.css';
+import Codox from '@codoxhq/quill-provider';
 
 function App() {
-
-  const { Codox } = window;
-
   const apiKey = '58e429b0-be4a-4cd8-8c8d-9a37fb0adec0';
   const username = 'Chris';
   const [docs, updateDocs] = useState([
-    { "id": "9d52d64b-f7c5-40de-8137-629376ffbc76", name: "doc1", "content": "Hello World" },
-    { "id": "9d52d64b-f7c5-40de-8137-629376ffbc77", name: "doc2", "content": "One two three" }
-  ])
-  const [activeDoc, setActiveDoc] = useState(docs[0])
-  const [codox, setCodoxInstance] = useState(null)
+    { id: '9d52d64b-f7c5-40de-8137-629376ffbc76', name: 'doc1', content: 'Hello World' },
+    { id: '9d52d64b-f7c5-40de-8137-629376ffbc77', name: 'doc2', content: 'One two three' },
+  ]);
+  const [activeDoc, setActiveDoc] = useState(docs[0]);
+  const [codox, setCodoxInstance] = useState(null);
 
   useEffect(() => {
-    const codoxInstance = new Codox()
-    setCodoxInstance(codoxInstance)
-  }, [])
+    const codoxInstance = new Codox();
+    setCodoxInstance(codoxInstance);
+  }, []);
 
   const changeActiveDoc = (doc) => {
-    
     if (doc.id !== activeDoc.id) {
       //leave the session
       codox && codox.stop();
 
       //create a new codox instance
-      const codoxInstance = new Codox()
-      setCodoxInstance(codoxInstance)
-      setActiveDoc(doc)
+      const codoxInstance = new Codox();
+      setCodoxInstance(codoxInstance);
+      setActiveDoc(doc);
     }
-
-  }
+  };
 
   const updateContent = (docId, content) => {
-    const updatedDoc = docs.map(doc => {
-      if(doc.id === docId){
-        return ({...doc, content})
+    const updatedDoc = docs.map((doc) => {
+      if (doc.id === docId) {
+        return { ...doc, content };
       }
-      return doc
-    })
-    updateDocs(updatedDoc)
-  }
-
+      return doc;
+    });
+    updateDocs(updatedDoc);
+  };
 
   return (
     <div className="App">
@@ -58,28 +53,27 @@ function App() {
         <div className="main-container">
           <div className="document-container">
             <ul className="document-list">
-              {docs.map(d =>
+              {docs.map((d) => (
                 <li key={d.id} onClick={() => changeActiveDoc(d)}>
                   <a className={`document-link ${activeDoc.id === d.id && 'active'}`}>{d.name}</a>
                 </li>
-              )}
-
+              ))}
             </ul>
           </div>
           <div className="editors">
-            {activeDoc.id ? <RealTimeQuillEditor
-              username={username}
-              apiKey={apiKey}
-              codox={codox}
-              docId={activeDoc.id}
-              model={activeDoc.content}
-              updateContent={updateContent}
-            /> : null}
-
+            {activeDoc.id ? (
+              <RealTimeQuillEditor
+                username={username}
+                apiKey={apiKey}
+                codox={codox}
+                docId={activeDoc.id}
+                model={activeDoc.content}
+                updateContent={updateContent}
+              />
+            ) : null}
           </div>
         </div>
       </div>
-
     </div>
   );
 }
--- react-quill-codox/src/components/RealTimeQuillEditor.js
+++ react-quill-codox/src/components/RealTimeQuillEditor.js
@@ -1,45 +1,38 @@
-
-import { useEffect, useRef } from 'react'
+import { useEffect, useRef } from 'react';
 import ReactQuill from 'react-quill';
 import 'react-quill/dist/quill.snow.css';
 
 const RealTimeQuillEditor = ({ codox, docId, username, apiKey, model, updateContent }) => {
-    const quillRef = useRef(null);
-    const editor = quillRef.current && quillRef.current.getEditor();
+  const quillRef = useRef(null);
+  const editor = quillRef.current && quillRef.current.getEditor();
 
-    
-    useEffect(() => {
+  useEffect(() => {
+    // initialization of codox and passing editor object
+    codox &&
+      codox.init({
+        app: 'quilljs',
+        username: username,
+        docId: docId,
+        apiKey: apiKey,
+        editor: editor,
+        hooks: {
+          // invoked whenever the document has been updated
+          contentChanged: () => {
+            const content = editor.root.innerHTML;
+            updateContent(docId, content);
+          },
+        },
+      });
 
-        // initialization of codox and passing editor object
-        codox && codox.init({
-            app: 'quilljs',
-            username: username,
-            docId: docId,
-            apiKey: apiKey,
-            editor: editor,
-            hooks: {
-                // invoked whenever the document has been updated
-                contentChanged: () => {
-                    const content = editor.root.innerHTML;
-                    updateContent(docId, content);
-                },
-            },
-        });
+    //leave the session
+    return () => codox && codox.stop();
+  }, [editor, docId]);
 
-        //leave the session
-        return () => codox && codox.stop();
-    }, [editor, docId])
+  return (
+    <>
+      <ReactQuill ref={quillRef} style={{ maxHeight: 100, width: 800 }} value={model} />
+    </>
+  );
+};
 
-    return (
-        <>
-            <ReactQuill
-                ref={quillRef}
-                style={{ maxHeight: 100, width: 800 }}
-                value={model}
-            />
-        </>
-
-    )
-}
-
-export default RealTimeQuillEditor
+export default RealTimeQuillEditor;
